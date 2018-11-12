0a688ff57dc4f8a826a84e8238f5dcfe4b011f2a
sr_hosseini <rassoulhosseini@gmail.com>
2018-11-12 17:59:14 +0330

    priority queue works fine

--- recieve.php
+++ recieve.php
@@ -1,9 +1,13 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
+
 use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Wire\AMQPTable;
+
 $connection = new AMQPStreamConnection('localhost', 5672, 'admin', 'admin');
 $channel = $connection->channel();
-$channel->queue_declare('hello', false, false, false, false);
+$channel->basic_qos(0, 1, false);
+//$channel->queue_declare('hello', false, false, false, false, false, new AMQPTable(['x-max-priority', 10]));
 echo " [*] Waiting for messages. To exit press CTRL+C\n";
 $callback = function ($msg) {
     echo ' [x] Received ', $msg->body, "\n";
--- send.php
+++ send.php
@@ -1,11 +1,18 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
+
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
+use PhpAmqpLib\Wire\AMQPTable;
+
 $connection = new AMQPStreamConnection('localhost', 5672, 'admin', 'admin');
 $channel = $connection->channel();
-$channel->queue_declare('hello', false, false, false, false);
-$msg = new AMQPMessage($argv[1] ?? 'Hello World!');
+$tbl = new AMQPTable();
+$tbl->set('x-max-priority', 10);
+$channel->queue_declare('hello', false, false, false, false, false, $tbl);
+
+$msg = new AMQPMessage($argv[1] ?? 'Hello World!', ['priority' => $argv[2] ?? 0]);
+
 echo sprintf('[x] Sent "%d"%s', $msg->body, "\n");
 $channel->basic_publish($msg, '', 'hello');
 $channel->close();
